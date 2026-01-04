c970aee55bc02577a7fde807e87958b6b06cb6d9
EXP <289065406@qq.com>
2026-01-04 23:33:01 +0800

    支持排序

--- py/gen_music_list.py
+++ py/gen_music_list.py
@@ -167,6 +167,10 @@ class MusicList:
         return len(self.item)
 
     def save_to_file(self, file_path):
+        # 根据 url 字段排序歌曲列表
+        sorted_items = sorted(self.item, key=lambda x: x.get('url', ''))
+        self.item = sorted_items
+        
         with open(file_path, 'w+', encoding=DEFAULT_ENCODING) as file:
             json.dump([self.__dict__], file, ensure_ascii=False, indent=4)
 
